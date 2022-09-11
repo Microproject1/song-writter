5767e1cd0665618a537657d6f0d1cb2c2b0770a4
Microproject1 <113281019+Microproject1@users.noreply.github.com>
2022-09-11 19:31:25 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,39 @@
-let sounds_on = 0
-if (sounds_on) {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
+let i_wont_give_up = 0
+let sounds_on = Math.randomBoolean()
+if (i_wont_give_up) {
+    music.playTone(415, music.beat(BeatFraction.Whole))
+    music.playTone(370, music.beat(BeatFraction.Whole))
     music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(370, music.beat(BeatFraction.Quarter))
+    music.playTone(415, music.beat(BeatFraction.Eighth))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(277, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(370, music.beat(BeatFraction.Double))
+    music.playTone(415, music.beat(BeatFraction.Eighth))
+    music.playTone(370, music.beat(BeatFraction.Quarter))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(277, music.beat(BeatFraction.Quarter))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(370, music.beat(BeatFraction.Quarter))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(277, music.beat(BeatFraction.Eighth))
+    music.playTone(247, music.beat(BeatFraction.Double))
+} else if (false) {
+	
 } else {
-    music.playTone(262, music.beat(BeatFraction.Whole))
+	
 }
 basic.forever(function () {
-    music.playMelody("- - - - - - - - ", 120)
+	
 })
