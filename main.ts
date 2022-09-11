fab8299777711b606de0f67d2aa08203ccccdc8d
Microproject1 <113281019+Microproject1@users.noreply.github.com>
2022-09-11 20:11:25 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,44 @@
-let sounds_on = Math.randomBoolean()
-if (true) {
-    music.playTone(415, music.beat(BeatFraction.Whole))
-    music.playTone(370, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(370, music.beat(BeatFraction.Quarter))
-    music.playTone(415, music.beat(BeatFraction.Eighth))
-    music.playTone(370, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(277, music.beat(BeatFraction.Double))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(370, music.beat(BeatFraction.Double))
-    music.playTone(415, music.beat(BeatFraction.Eighth))
-    music.playTone(370, music.beat(BeatFraction.Quarter))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(277, music.beat(BeatFraction.Quarter))
-    music.playTone(247, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(370, music.beat(BeatFraction.Quarter))
-    music.playTone(370, music.beat(BeatFraction.Half))
-    music.playTone(277, music.beat(BeatFraction.Eighth))
-    music.playTone(247, music.beat(BeatFraction.Double))
-} else if (false) {
-	
-} else {
-	
-}
+let sounds_on = false
+input.onButtonPressed(Button.A, function () {
+    sounds_on = Math.randomBoolean()
+    if (true) {
+        music.playTone(415, music.beat(BeatFraction.Whole))
+        music.playTone(370, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Double))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(370, music.beat(BeatFraction.Whole))
+        music.playTone(415, music.beat(BeatFraction.Half))
+        music.playTone(370, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(277, music.beat(BeatFraction.Double))
+        music.playTone(330, music.beat(BeatFraction.Double))
+        music.playTone(370, music.beat(BeatFraction.Double))
+        music.playTone(415, music.beat(BeatFraction.Half))
+        music.playTone(370, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(277, music.beat(BeatFraction.Half))
+        music.playTone(247, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Double))
+        music.playTone(330, music.beat(BeatFraction.Double))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(370, music.beat(BeatFraction.Half))
+        music.playTone(370, music.beat(BeatFraction.Whole))
+        music.playTone(277, music.beat(BeatFraction.Half))
+        music.playTone(247, music.beat(BeatFraction.Double))
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
 basic.forever(function () {
 	
 })
