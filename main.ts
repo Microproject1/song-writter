17cdd71452814f14e5aafedb401fbfcb410eff95
Microproject1 <113281019+Microproject1@users.noreply.github.com>
2022-09-11 18:44:43 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,13 @@
-input.onSound(DetectedSound.Loud, function () {
-    sounds_on = !(sounds_on)
-    if (sounds_on) {
-    	
-    } else {
-    	
-    }
-})
-let sounds_on = false
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(294, music.beat(BeatFraction.Whole))
-music.playTone(330, music.beat(BeatFraction.Whole))
-music.playTone(349, music.beat(BeatFraction.Whole))
-music.playTone(392, music.beat(BeatFraction.Whole))
+let sounds_on = 0
+if (sounds_on) {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+} else {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+}
 basic.forever(function () {
-    music.ringTone(262)
+	
 })
