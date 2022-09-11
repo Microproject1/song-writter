16cbf1a6133889bd39c1a888ac497c83f39812c6
Microproject1 <113281019+Microproject1@users.noreply.github.com>
2022-09-11 16:59:11 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
 music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
-music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(294, music.beat(BeatFraction.Whole))
+music.playTone(330, music.beat(BeatFraction.Whole))
+music.playTone(349, music.beat(BeatFraction.Whole))
 basic.forever(function () {
-    if (true) {
-    	
-    } else {
-    	
-    }
+	
 })
